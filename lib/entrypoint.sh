48523498abbdc7342c6c0133f8fea6571fc42313
Oguz Bilgic <fisyonet@gmail.com>
2019-10-05 23:19:46 -0700

    Remove echo from entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-echo $GITHUB_ACTION
-echo $GITHUB_EVENT_NAME
-echo $GITHUB_WORKFLOW
-echo $GITHUB_WORKSPACE
-
 npm install --no-package-lock
 
 NODE_PATH=node_modules node /action/lib/run.js
