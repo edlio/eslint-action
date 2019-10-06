8cf66a1eea669fcdf8b669aa21233fa93bf8486e
Oguz Bilgic <fisyonet@gmail.com>
2019-10-05 20:00:52 -0700

    Change ulimit in entrypoint

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 cd /action/lib
 npm install
 
+# https://unix.stackexchange.com/questions/108174/how-to-persistently-control-maximum-system-resource-consumption-on-mac/293062#293062
+ulimit -n 1024
+ulimit -u 1024
+
 echo $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
 npm install
