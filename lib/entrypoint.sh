b5a92b9dbc34b8c0dd53d28a156a1688d8a916aa
Oguz Bilgic <fisyonet@gmail.com>
2019-10-05 21:16:37 -0700

    Echo env vars

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+echo $GITHUB_ACTION
+echo $GITHUB_EVENT_NAME
+echo $GITHUB_WORKFLOW
 echo $GITHUB_WORKSPACE
-cd $GITHUB_WORKSPACE
+
 npm install --no-package-lock
 
 NODE_PATH=node_modules node /action/lib/run.js
