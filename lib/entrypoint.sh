d69c06109d0a3d2d95a51242cf0cb6f199e324a5
Oguz Bilgic <fisyonet@gmail.com>
2019-10-05 20:06:31 -0700

    No package lock

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -5,12 +5,8 @@ set -e
 cd /action/lib
 npm install
 
-# https://unix.stackexchange.com/questions/108174/how-to-persistently-control-maximum-system-resource-consumption-on-mac/293062#293062
-ulimit -n 1024
-ulimit -u 1024
-
 echo $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
-npm install
+npm install --no-package-lock
 
 NODE_PATH=node_modules node /action/lib/run.js
